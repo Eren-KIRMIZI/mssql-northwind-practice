c2f5fde69b1ed3d30dae627d46d862f5b6472d3c
Eren KIRMIZI <erencoding94@gmail.com>
2026-01-10 22:51:58 +0300

    Create trg_satis_eklendi trigger for new sales
    This trigger prints a message when a new sale is added to the Satislar table.

---
+++ 07_triggers/trg_satis_eklendi.sql
@@ -0,0 +1,27 @@
+/*
+Trigger     : trg_satis_eklendi
+Tablo       : dbo.Satislar
+Açıklama    : Yeni satış eklendiğinde tetiklenir
+*/
+
+USE Northwind;
+GO
+
+IF OBJECT_ID('dbo.trg_satis_eklendi', 'TR') IS NOT NULL
+    DROP TRIGGER dbo.trg_satis_eklendi;
+GO
+
+CREATE TRIGGER dbo.trg_satis_eklendi
+ON dbo.Satislar
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @SatisID int;
+
+    SELECT @SatisID = SatisID FROM inserted;
+
+    PRINT 'Yeni satış eklendi. SatisID: ' + CAST(@SatisID AS varchar);
+END;
+GO
